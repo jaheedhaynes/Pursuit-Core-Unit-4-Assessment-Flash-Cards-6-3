ca75007e2fa95fb9ad159787c3c382f6224932cb
Jaheed Haynes <jaheedhaynes@pursuit.org>
2020-02-11 11:04:01 -0500

    API Client

---
+++ Unit4Assessment/Unit4Assessment/APIClient.swift
@@ -0,0 +1,39 @@
+//
+//  APIClient.swift
+//  Unit4Assessment
+//
+//  Created by Jaheed Haynes on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import Foundation
+import NetworkHelper
+
+struct CardAPIClient {
+    static func fetchCard(for id: String, completion: @escaping (Result<[Card], AppError>) -> ()) {
+        
+        let endpointURLString = "https://5daf8b36f2946f001481d81c.mockapi.io/api/v2/cards"
+        
+        guard let url = URL(string: endpointURLString) else {
+            completion(.failure(.badURL(endpointURLString)))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let cardValue = try JSONDecoder().decode(Unit4.self, from: data)
+                    completion(.success(cardValue.cards))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
+
---
+++ Unit4Assessment/Unit4Assessment/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  Unit4Assessment
+//
+//  Created by Jaheed Haynes on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+
+struct Unit4: Codable {
+    let cardListType: String
+    let apiVersion: String
+    let cards: [Card]
+}
+
+
+struct Card: Codable {
+    let id: String
+    let cardTitle: String
+    let facts: [String]
+}
